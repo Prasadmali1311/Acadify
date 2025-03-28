6b9e9949462d74127a37de5cd9c18df2a505d297
Prasadmali1311 <75989113+Prasadmali1311@users.noreply.github.com>
2025-03-28 23:12:56 +0530

    Standardized css in all codebase-> created new separate, dedicated css file for each individual file.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import './styles.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './styles/global.css'
+import './components/Layout.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
